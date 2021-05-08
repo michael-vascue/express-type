d3e16594095614f71ae1778dd4770a3785f54124
Michael Fung <michael@vascue.io>
2021-05-08 17:29:49 +0800

    updated tsconfig

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import expressSession from "express-session";
 
 const app = express()
+app.use(expressSession())
 
 app.get("/", (req: express.Request, res: express.Response) => {
   req.session.user
+})
+
+app.listen(8080, ()=> {
+
 })
